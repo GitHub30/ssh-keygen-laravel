c52644e7088c485a6663aa28abb3451c4ed390bc
GitHub30 <funaox@gmail.com>
2024-08-16 14:13:10 +0000

    impl ssh-keygen

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Process;
 
 class GenerateController extends Controller
 {
@@ -11,6 +14,11 @@ class GenerateController extends Controller
      */
     public function __invoke()
     {
-        return 'hello';
+        $directory_path = public_path('keys' . DIRECTORY_SEPARATOR . Str::random(10));
+        File::makeDirectory($directory_path, recursive: true);
+        $file_path = $directory_path . DIRECTORY_SEPARATOR . 'id_rsa';
+        $password = '';
+        $result = Process::run("ssh-keygen -f '$file_path' -N '$password'");
+        return $result->output();
     }
 }
