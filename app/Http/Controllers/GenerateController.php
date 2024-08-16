9005b0abbef0b57de95730ee5d72366ee352d672
GitHub30 <funaox@gmail.com>
2024-08-16 13:18:04 +0000

    add controller

---
+++ app/Http/Controllers/GenerateController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GenerateController extends Controller
+{
+    /**
+     * Execute ssh-keygen
+     */
+    public function __invoke()
+    {
+        return 'hello';
+    }
+}
