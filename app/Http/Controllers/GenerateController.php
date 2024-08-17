76e7f5b6fe0ef8105b861c410256503d4433e257
GitHub30 <funaox@gmail.com>
2024-08-17 08:57:36 +0000

    add

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -6,19 +6,32 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Process;
+use App\Http\Requests\GenerateRequest;
 
 class GenerateController extends Controller
 {
     /**
      * Execute ssh-keygen
      */
-    public function __invoke()
+    public function __invoke(GenerateRequest $request)
     {
-        $directory_path = public_path('keys' . DIRECTORY_SEPARATOR . Str::random(10));
+        $f = $request->input('f', 'id_rsa');
+        $N = $request->input('N', '');
+        $t = $request->input('t');
+        $optional_args = '';
+        if ($t) {
+            $optional_args .= " -t '$t'";
+        }
+
+        $relative_dir_path = 'keys' . DIRECTORY_SEPARATOR . Str::random(10);
+        $relative_file_path = $relative_dir_path . DIRECTORY_SEPARATOR . $f;
+        $directory_path = public_path($relative_dir_path);
         File::makeDirectory($directory_path, recursive: true);
-        $file_path = $directory_path . DIRECTORY_SEPARATOR . 'id_rsa';
-        $password = '';
-        $result = Process::run("ssh-keygen -f '$file_path' -N '$password'");
-        return $result->output();
+        $file_path = public_path($relative_file_path);
+        Process::run("ssh-keygen -f '$file_path' -N '$N'$optional_args");
+        return [
+            'public_key' => ['path' => "$relative_file_path.pub", 'file' => File::get("$file_path.pub")],
+            'private_key' => ['path' => $relative_file_path, 'file' => File::get($file_path)]
+        ];
     }
 }
---
+++ app/Http/Requests/GenerateRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class GenerateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'f' => ['alpha_num:ascii'],
+            'N' => ['alpha_num:ascii'],
+            't' => ['in:rsa,dsa,ecdsa,ed25519'],
+        ];
+    }
+
+    /**
+     * Prevent redirect
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
